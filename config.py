f9eca08cbb2c2bfde3a66095c39afd815810c491
Addison <vxytllc@gmail.com>
2025-02-01 08:42:14 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Mastodon API credentials
+ACCESS_TOKEN = "your_access_token_here"
+
+# Other configuration options (optional)
+INSTANCE_URL = "https://mastodon.social"
+CLIENT_ID = "your_client_id_here"
+CLIENT_SECRET = "your_client_secret_here"
